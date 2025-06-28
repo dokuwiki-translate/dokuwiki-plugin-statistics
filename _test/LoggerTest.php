5cc1319aff2e462a6992d316613aa4c07e0d7b0c
Andreas Gohr <andi@splitbrain.org>
2025-06-28 19:48:52 +0200

    test: add LoggerTest.php for logging functionality testing

---
+++ _test/LoggerTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace dokuwiki\plugin\statistics\test;
+
+use DokuWikiTest;
+
+/**
+ * FIXME tests for the statistics plugin
+ *
+ * @group plugin_statistics
+ * @group plugins
+ */
+class LoggerTest extends DokuWikiTest
+{
+    /**
+     * @todo implement tests
+     */
+    public function testSomething()
+    {
+
+    }
+}
