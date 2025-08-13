6c5dec65792f8dd0cb5ec53c3da4a034a393f571
Andreas Gohr <gohr@cosmocode.de>
2025-08-13 09:41:57 +0200

    drop obsolete config

--- conf/default.php
+++ conf/default.php
@@ -4,9 +4,4 @@
  * @author Andreas Gohr <andi@splitbrain.org>
  */
 
-$conf['db_server']   = 'localhost';
-$conf['db_user']     = '';
-$conf['db_password'] = '';
-$conf['db_database'] = 'stats';
-$conf['db_prefix']   = 'stats_';
 $conf['loggroups']   = array();
--- conf/metadata.php
+++ conf/metadata.php
@@ -6,9 +6,4 @@
  * @author Andreas Gohr <andi@splitbrain.org>
  */
 
-$meta['db_server']   = array('string');
-$meta['db_user']     = array('string');
-$meta['db_password'] = array('password');
-$meta['db_database'] = array('string');
-$meta['db_prefix']   = array('string');
 $meta['loggroups']   = array('array');
