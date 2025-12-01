df19f3f7ff425a4e7510d013b63f2db4d401460f
Andreas Gohr <gohr@cosmocode.de>
2025-12-01 15:03:37 +0100

    rename lib to js
    This fixes an issue with the dokuwiki github action

--- admin.php
+++ admin.php
@@ -144,8 +144,8 @@ class admin_plugin_statistics extends AdminPlugin
      */
     public function html()
     {
-        echo '<script src="' . DOKU_BASE . 'lib/plugins/statistics/lib/chart.js"></script>';
-        echo '<script src="' . DOKU_BASE . 'lib/plugins/statistics/lib/chartjs-plugin-datalabels.js"></script>';
+        echo '<script src="' . DOKU_BASE . 'lib/plugins/statistics/js/chart.js"></script>';
+        echo '<script src="' . DOKU_BASE . 'lib/plugins/statistics/js/chartjs-plugin-datalabels.js"></script>';
 
         echo '<div id="plugin__statistics">';
         echo '<h1>' . $this->getLang('menu') . '</h1>';
